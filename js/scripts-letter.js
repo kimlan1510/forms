a7ac6b9da15643991780f66eaeadaa16af65a62e
kimlan nguyen and Michelle Poterek <michelle@>
2017-04-26 10:42:37 -0700

    add panel and background color to panel.

--- js/scripts-letter.js
+++ js/scripts-letter.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $("#postcard").submit(function(event){
-    // var user_name = $("input#user-name").val();
-    // var user_address = $("input#user-address").val();
-    // $("input#user-name").val();
-    $(".user-name").append("abc");
-    // $(".user-address").text(user_address);
+    var user_name = $("input#user-name").val();
+    var user_address = $("input#user-address").val();
+      
+    $(".user-name").text(user_name);
+    $(".user-address").text(user_address);
 
     $(".content").show();
     event.preventDefault();
