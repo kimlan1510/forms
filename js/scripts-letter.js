290f83b56c0b5e47954adb9d43c133523ec4d17f
kimlan nguyen and Michelle Poterek <kimlan@>
2017-04-26 10:34:55 -0700

    add button to submit form letter

--- js/scripts-letter.js
+++ js/scripts-letter.js
@@ -1,1 +1,13 @@
-$(function)
+$(document).ready(function() {
+  $("#postcard").submit(function(event){
+    // var user_name = $("input#user-name").val();
+    // var user_address = $("input#user-address").val();
+    // $("input#user-name").val();
+    $(".user-name").append("abc");
+    // $(".user-address").text(user_address);
+
+    $(".content").show();
+    event.preventDefault();
+  });
+
+});
